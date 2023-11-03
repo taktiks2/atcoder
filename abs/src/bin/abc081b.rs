5369a3ba43c039102b745146b48965df3fe3ff55
taktiks2 <deathproof.lee@gmail.com>
2023-11-03 10:32:21 +0900

    abc081b

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,6 +1,16 @@
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        mut a: [i32; n],
     };
+
+    let mut cnt = 0;
+
+    while a.iter().all(|&x| x % 2 == 0) {
+        a = a.iter().map(|&x| x / 2).collect();
+        cnt += 1;
+    }
+    println!("{}", cnt);
 }
