77ed98b7520c6152e4e0fda439b040575c4d5565
taktiks2 <deathproof.lee@gmail.com>
2023-11-03 15:48:20 +0900

    abc088b

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,6 +1,22 @@
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        n: u32,
+        mut a: [u32; n],
     };
+
+    let mut alice = 0;
+    let mut bob = 0;
+
+    a.sort_by(|a, b| b.cmp(a)); // 降順にソート
+    for (index, element) in a.iter().enumerate() {
+        if index % 2  == 1 {
+            bob += element;
+        } else {
+            alice += element;
+        }
+    }
+
+    println!("{}", alice - bob);
 }
