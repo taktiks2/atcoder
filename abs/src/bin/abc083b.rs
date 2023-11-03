05d93dfaf9a4cd1fa2f833f38fd7ac6350e6c9a2
taktiks2 <deathproof.lee@gmail.com>
2023-11-03 11:47:52 +0900

    abc083b

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,6 +1,21 @@
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        nab: [u32; 3],
     };
+
+    let mut result = 0;
+
+    for n in 1..=nab[0] {
+        let mut ttl = 0;
+        for nc in n.to_string().as_str().chars() {
+            ttl += nc.to_digit(10).unwrap();
+        }
+        if ttl >= nab[1] && ttl <= nab[2] {
+            result += n;
+        }
+    }
+
+    println!("{}", result);
 }
