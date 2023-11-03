27ad92e305bc38e721bce8b1b7325cdf78bb8696
taktiks2 <deathproof.lee@gmail.com>
2023-11-03 19:43:41 +0900

    abc085c

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,6 +1,21 @@
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        n: i32,
+        ttl: i32,
     };
+
+    let mut ans = String::from("-1 -1 -1");
+
+    for x in 0..=n {
+        for y in 0..=(n - x) {
+            let z = n - x - y;
+            if x * 10000 + y * 5000 + z * 1000 == ttl {
+                ans = format!("{} {} {}", x, y, z);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
