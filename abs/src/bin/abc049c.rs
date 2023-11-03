488ad1df03b2f4d60a097e4e5ee075e5d1c77a73
taktiks2 <deathproof.lee@gmail.com>
2023-11-03 22:17:59 +0900

    abc049c

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,6 +1,20 @@
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        mut s: String,
     };
+
+    let mut ans = "NO";
+
+    s = s.replace("eraser", "");
+    s = s.replace("erase", "");
+    s = s.replace("dreamer", "");
+    s = s.replace("dream", "");
+
+    if s.is_empty() {
+        ans = "YES";
+    }
+
+    println!("{}", ans);
 }
