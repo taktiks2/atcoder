8c4f9f95c607640e95ed80ce788d54656dbab5df
taktiks2 <deathproof.lee@gmail.com>
2023-11-04 01:41:10 +0900

    004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,6 +1,35 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        m: [[u32; w]; h],
     };
+
+    let mut ans = vec![vec![0; w]; h];
+
+    for hi in 0..h {
+        let row_sum: u32 = m[hi].iter().sum();
+
+        for wj in 0..w {
+            let mut ttl = 0;
+
+            let col_sum = m.iter().map(|row| row[wj]).sum::<u32>();
+            ttl += col_sum + row_sum - m[hi][wj];
+
+            ans[hi][wj] = ttl;
+        }
+    }
+
+    for row in ans {
+        for (i, val) in row.iter().enumerate() {
+            if i == row.len() - 1 {
+                print!("{}", val);
+            } else {
+                print!("{} ", val);
+            }
+        }
+        println!();
+    }
 }
