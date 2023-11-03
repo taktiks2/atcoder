fd77e14b919bd1a230f08f16e78814f6873cf70e
taktiks2 <deathproof.lee@gmail.com>
2023-11-03 10:32:29 +0900

    abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,6 +1,27 @@
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        x: i32,
     };
+
+    let mut cnt = 0;
+
+    for i in 0..=a {
+        for j in 0..=b {
+            let remain_amount = x - (i * 500 + j * 100);
+            if remain_amount == 0 {
+                cnt += 1;
+            } else if remain_amount < 50 {
+                break;
+            } else if remain_amount % 50 == 0 && remain_amount / 50 <= c {
+                cnt += 1;
+            }
+        }
+    }
+
+    println!("{}", cnt)
 }
