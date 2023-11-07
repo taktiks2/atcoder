a15d7104c27c287128bca41402252125efaa9f6c
taktiks2 <deathproof.lee@gmail.com>
2023-11-07 23:44:18 +0900

    022 AC

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,6 +1,33 @@
-use proconio::{ input };
+use proconio::input;
+
+fn calc_gcd(m: i64, n: i64) -> i64 {
+    if n == 0 {
+        m
+    } else {
+        calc_gcd(n, m % n)
+    }
+}
 
 fn main() {
     input! {
+        a: i64,
+        b: i64,
+        c: i64,
     };
+
+    let mut ab_gcd = 0;
+    if a > b {
+        ab_gcd = calc_gcd(a, b);
+    } else {
+        ab_gcd = calc_gcd(b, a);
+    }
+
+    let mut abc_gcd = 0;
+    if ab_gcd > c {
+        abc_gcd = calc_gcd(ab_gcd, c);
+    } else {
+        abc_gcd = calc_gcd(c, ab_gcd);
+    }
+
+    println!("{}", a / abc_gcd + b / abc_gcd + c / abc_gcd - 3)
 }
