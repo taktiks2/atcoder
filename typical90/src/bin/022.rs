d37d8c93df3c604bb858057ff144a77fdf5b2ecb
taktiks2 <deathproof.lee@gmail.com>
2023-11-07 23:51:58 +0900

    022 AC

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,10 +1,10 @@
 use proconio::input;
 
-fn calc_gcd(m: i64, n: i64) -> i64 {
-    if n == 0 {
-        m
+fn gcd(m: i64, n: i64) -> i64 {
+    if m % n == 0 {
+        n
     } else {
-        calc_gcd(n, m % n)
+        gcd(n, m % n)
     }
 }
 
@@ -15,19 +15,7 @@ fn main() {
         c: i64,
     };
 
-    let mut ab_gcd = 0;
-    if a > b {
-        ab_gcd = calc_gcd(a, b);
-    } else {
-        ab_gcd = calc_gcd(b, a);
-    }
+    let g = gcd(gcd(a, b), c);
 
-    let mut abc_gcd = 0;
-    if ab_gcd > c {
-        abc_gcd = calc_gcd(ab_gcd, c);
-    } else {
-        abc_gcd = calc_gcd(c, ab_gcd);
-    }
-
-    println!("{}", a / abc_gcd + b / abc_gcd + c / abc_gcd - 3)
+    println!("{}", (a + b + c) / g - 3)
 }
