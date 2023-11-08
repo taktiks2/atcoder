cd4ac4b66ee7ccfcf0c9c70964c079bce706aff2
taktiks2 <deathproof.lee@gmail.com>
2023-11-09 00:04:43 +0900

    024 AC

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -1,6 +1,24 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: i32,
+        a: [i32; n],
+        b: [i32; n],
     };
+
+    let mut ans = "No";
+
+    let mut diff = 0;
+    for i in 0..n {
+        diff += (a[i] - b[i]).abs()
+    }
+
+
+    if diff <= k && k % 2 == diff % 2 {
+        ans = "Yes";
+    }
+
+    println!("{}", ans);
 }
