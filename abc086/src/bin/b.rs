9c22f8bafa343b8e250876cfa52e1d1348e0dabd
taktiks2 <deathproof.lee@gmail.com>
2023-11-08 23:23:36 +0900

    b AC

--- abc086/src/bin/b.rs
+++ abc086/src/bin/b.rs
@@ -1,6 +1,26 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        mut a: String,
+        b: String,
     };
+
+    a.push_str(&b);
+
+    let n: i32 = a.parse().unwrap();
+
+    let mut ans = "No";
+    let mut i = 1;
+    loop {
+        if i * i > n {
+            break;
+        } else if i * i == n {
+            ans = "Yes";
+            break;
+        }
+        i += 1;
+    }
+
+    println!("{}", ans)
 }
