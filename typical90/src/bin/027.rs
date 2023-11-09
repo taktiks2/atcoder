ab510d7663b3c7281289145b7c0a96349b684db4
taktiks2 <deathproof.lee@gmail.com>
2023-11-09 23:35:41 +0900

    027 TLE

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,6 +1,24 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        s: [String; n],
     };
+
+    let mut ans: Vec<usize> = vec![];
+    let mut users: Vec<String> = vec![];
+
+    for i in 0..n {
+        if users.iter().any(|user| user == &s[i]) {
+            continue;
+        } else {
+            ans.push(i + 1);
+            users.push(s[i].clone());
+        }
+    }
+
+    for a in ans {
+        println!("{}", a);
+    }
 }
