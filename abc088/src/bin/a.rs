389f21ff88eafd6e90b103ab2e03648dad7e9cb8
taktiks2 <deathproof.lee@gmail.com>
2023-11-10 22:38:35 +0900

    a AC

--- abc088/src/bin/a.rs
+++ abc088/src/bin/a.rs
@@ -1,6 +1,16 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: i32,
+        a: i32,
     };
+
+    let mut ans = "No";
+
+    if n % 500 <= a {
+        ans = "Yes";
+    }
+
+    println!("{}", ans)
 }
