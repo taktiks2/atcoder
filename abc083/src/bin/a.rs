12e398bbc3fc909e8182ef613aa84f4080b18a98
taktiks2 <deathproof.lee@gmail.com>
2023-11-12 00:23:37 +0900

    abc083 a AC

--- abc083/src/bin/a.rs
+++ abc083/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{ input };
+use proconio::input;
+use std::cmp::Ordering;
 
 fn main() {
     input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
     };
+
+    match (a + b).cmp(&(c + d)) {
+        Ordering::Less => println!("Right"),
+        Ordering::Equal => println!("Balanced"),
+        Ordering::Greater => println!("Left"),
+    }
 }
