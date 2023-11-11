8f4d9124fa9846efd7513935a8195ba692395e65
taktiks2 <deathproof.lee@gmail.com>
2023-11-11 23:50:58 +0900

    a AC

--- abc328/src/bin/a.rs
+++ abc328/src/bin/a.rs
@@ -1,6 +1,13 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: i32,
+        x: i32,
+        s: [i32; n],
     };
+
+    let ans: i32 = s.iter().filter(|&num| num <= &x).sum();
+
+    println!("{}", ans);
 }
