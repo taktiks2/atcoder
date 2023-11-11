9bea096506822c0110a7fe49ed200588573884f8
taktiks2 <deathproof.lee@gmail.com>
2023-11-11 23:51:32 +0900

    c WA

--- abc328/src/bin/c.rs
+++ abc328/src/bin/c.rs
@@ -1,6 +1,21 @@
-use proconio::{ input };
+use proconio::input;
+use regex::Regex;
 
 fn main() {
     input! {
+        n: usize,
+        q: usize,
+        s: String,
+        lr: [(i32, i32); q],
     };
+
+    let re = Regex::new(r"[a-z]{2}").unwrap();
+
+    for i in 0..q {
+        let (l, r) = lr[i];
+        let ss: &str = &s;
+        let part = &ss[(l as usize)..=(r as usize)];
+
+        println!("{}", re.find_iter(&part).count());
+    }
 }
