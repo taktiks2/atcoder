536ac88374cc5bc4124eaade5e0a250d4ffbbe0a
taktiks2 <deathproof.lee@gmail.com>
2023-11-11 23:51:08 +0900

    b AC

--- abc328/src/bin/b.rs
+++ abc328/src/bin/b.rs
@@ -1,6 +1,22 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        d: [i32; n],
     };
+
+    let mut total = 0;
+
+    for i in 1..=n {
+        for j in 1..=d[i - 1] {
+            let num = format!("{}{}", i, j);
+
+            if num.chars().all(|c| c == num.chars().nth(0).unwrap()) {
+                total += 1;
+            }
+        }
+    }
+
+    println!("{}", total)
 }
