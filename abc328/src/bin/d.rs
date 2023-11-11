1387a07f24873a73cb7f8e951261889187bc3609
taktiks2 <deathproof.lee@gmail.com>
2023-11-11 23:51:38 +0900

    d TLE

--- abc328/src/bin/d.rs
+++ abc328/src/bin/d.rs
@@ -1,6 +1,21 @@
-use proconio::{ input };
+use proconio::input;
+
+const TARGET: &str = "ABC";
 
 fn main() {
     input! {
+        mut s: String,
     };
+
+    loop {
+        if s.contains(TARGET) {
+            s = s.replace(TARGET, "")
+        } else {
+            break;
+        }
+    }
+
+    if s.len() != 0 {
+        println!("{}", s);
+    }
 }
