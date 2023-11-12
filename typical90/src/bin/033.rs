146a30c9c22c8d8f4dc0a2a7d4095f2c839b2f53
taktiks2 <deathproof.lee@gmail.com>
2023-11-12 17:59:41 +0900

    typical90 033 AC

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -1,6 +1,15 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        h: i32,
+        w: i32,
     };
+
+    if h == 1 || w == 1 {
+        println!("{}", h * w);
+        return;
+    }
+
+    println!("{}", ((h + 1) / 2) * ((w + 1) / 2));
 }
