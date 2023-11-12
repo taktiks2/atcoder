bf72059580ec535b645cdde42af74712f978b719
taktiks2 <deathproof.lee@gmail.com>
2023-11-13 00:01:22 +0900

    typical90 055 WA

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -1,6 +1,28 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        p: u64,
+        q: u64,
+        a: [u64; n],
     };
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        for j in 0..i {
+            for k in 0..j {
+                for l in 0..k {
+                    for m in 0..l {
+                        if a[i] * a[j] * a[k] * a[l] * a[m] % p == q {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", ans)
 }
