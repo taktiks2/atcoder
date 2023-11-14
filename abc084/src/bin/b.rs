101c48c565d62bd6d7a557529ac1829a156c3495
taktiks2 <deathproof.lee@gmail.com>
2023-11-14 22:33:15 +0900

    abc084 b AC

--- abc084/src/bin/b.rs
+++ abc084/src/bin/b.rs
@@ -1,6 +1,29 @@
-use proconio::{ input };
+use proconio::input;
+use regex::Regex;
 
 fn main() {
     input! {
+        a: usize,
+        _: i32,
+        s: String,
     };
+
+    let re = Regex::new(r"^\d+$").unwrap();
+    let mut new_s = String::new();
+
+    for (i, c) in s.chars().enumerate() {
+        if i == a && c != '-' {
+            println!("No");
+            return;
+        }
+        if i != a {
+            new_s.push(c);
+        }
+    }
+
+    if re.is_match(&new_s) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
