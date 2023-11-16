47bbc9265765a43149a9a817949802184b519acf
taktiks2 <deathproof.lee@gmail.com>
2023-11-16 23:06:53 +0900

    abc326 a AC

--- abc326/src/bin/a.rs
+++ abc326/src/bin/a.rs
@@ -1,6 +1,24 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        x: i32,
+        y: i32,
     };
+
+    let diff = x - y;
+
+    if diff > 0 {
+        if diff < 4 {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    } else {
+        if diff > -3 {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
