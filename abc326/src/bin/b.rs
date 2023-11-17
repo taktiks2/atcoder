ec816e162c0fe3f02afa4001cc437ffa8857d4f9
taktiks2 <deathproof.lee@gmail.com>
2023-11-17 20:34:58 +0900

    abc326 b AC

--- abc326/src/bin/b.rs
+++ abc326/src/bin/b.rs
@@ -1,6 +1,19 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: i32,
     };
+
+    for i in n..=919 {
+        let i = i.to_string();
+        let hundleds = i.chars().next().unwrap().to_digit(10).unwrap();
+        let tens = i.chars().nth(1).unwrap().to_digit(10).unwrap();
+        let one = i.chars().nth(2).unwrap().to_digit(10).unwrap();
+
+        if hundleds * tens == one {
+            println!("{}", i);
+            break;
+        }
+    }
 }
