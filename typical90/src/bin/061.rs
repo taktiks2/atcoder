e48b96dcc0e137622866df6d42ecaa62f284c1b2
taktiks2 <deathproof.lee@gmail.com>
2023-11-18 14:02:57 +0900

    typical90 061 AC

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,6 +1,19 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        q: usize,
+        tx: [(i32, i32); q],
     };
+
+    let mut deck: Vec<i32> = vec![];
+
+    for (t, x) in tx {
+        match t {
+            1 => deck.insert(0, x),
+            2 => deck.push(x),
+            3 => println!("{}", deck[(x - 1) as usize]),
+            _ => (),
+        }
+    }
 }
