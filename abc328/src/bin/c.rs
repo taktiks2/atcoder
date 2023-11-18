8ae56c7173959f67b5f9812970db11aadf3b26e8
taktiks2 <deathproof.lee@gmail.com>
2023-11-18 15:08:10 +0900

    abc328 c TLE

--- abc328/src/bin/c.rs
+++ abc328/src/bin/c.rs
@@ -1,21 +1,25 @@
 use proconio::input;
-use regex::Regex;
 
 fn main() {
     input! {
-        n: usize,
+        _: usize,
         q: usize,
         s: String,
         lr: [(i32, i32); q],
     };
 
-    let re = Regex::new(r"[a-z]{2}").unwrap();
-
-    for i in 0..q {
-        let (l, r) = lr[i];
+    for (l, r) in lr {
         let ss: &str = &s;
-        let part = &ss[(l as usize)..=(r as usize)];
+        let part = &ss[((l - 1) as usize)..(r as usize)];
 
-        println!("{}", re.find_iter(&part).count());
+        let mut cnt = 0;
+        let mut prev_c: char = ' ';
+        for c in part.chars() {
+            if c == prev_c {
+                cnt += 1;
+            }
+            prev_c = c;
+        }
+        println!("{}", cnt);
     }
 }
