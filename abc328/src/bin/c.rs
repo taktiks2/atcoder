f98c4795de1d67751245adf2fbfd6f5d3e767ec1
taktiks2 <deathproof.lee@gmail.com>
2023-11-20 23:22:15 +0900

    abc328 c AC

--- abc328/src/bin/c.rs
+++ abc328/src/bin/c.rs
@@ -2,24 +2,36 @@ use proconio::input;
 
 fn main() {
     input! {
-        _: usize,
+        n: usize,
         q: usize,
         s: String,
         lr: [(i32, i32); q],
     };
 
-    for (l, r) in lr {
-        let ss: &str = &s;
-        let part = &ss[((l - 1) as usize)..(r as usize)];
+    let mut ss = vec![];
+    for c in s.chars() {
+        ss.push(c);
+    }
 
-        let mut cnt = 0;
-        let mut prev_c: char = ' ';
-        for c in part.chars() {
-            if c == prev_c {
-                cnt += 1;
-            }
-            prev_c = c;
+    let mut a = vec![];
+    for i in 0..n {
+        if i > 0 && ss[i] == ss[i - 1] {
+            a.push(1);
+        } else {
+            a.push(0);
         }
-        println!("{}", cnt);
+    }
+
+    let mut b = vec![0; a.len()];
+    for i in 0..a.len() {
+        if i == 0 {
+            b[i] = a[i];
+            continue;
+        }
+        b[i] = b[i - 1] + a[i];
+    }
+
+    for (l, r) in lr {
+        println!("{}", b[r as usize - 1] - b[l as usize - 1]);
     }
 }
