54e5f376551248f5757b88f3dd0a316fb9ec89e8
taktiks2 <deathproof.lee@gmail.com>
2023-11-20 23:22:41 +0900

    abc329

---
+++ abc329/src/bin/c.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        s: String,
+    };
+
+    let mut groups: Vec<String> = Vec::new();
+    let mut current_char: Option<char> = None;
+
+    for i in 0..n {}
+}
