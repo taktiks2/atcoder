e794c36e70804e71521173d49673aa0df9ebd6d3
taktiks2 <deathproof.lee@gmail.com>
2023-11-22 08:32:32 +0900

    abc063 b AC

--- abc063/src/bin/b.rs
+++ abc063/src/bin/b.rs
@@ -1,6 +1,23 @@
-use proconio::{ input };
+use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
+        s: String,
     };
+
+    let len = s.len();
+
+    let mut chars = HashSet::new();
+
+    for c in s.chars() {
+        chars.insert(c);
+    }
+
+    let mut ans = "no";
+    if len == chars.len() {
+        ans = "yes";
+    }
+
+    println!("{}", ans);
 }
