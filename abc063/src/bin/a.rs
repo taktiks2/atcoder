d486366c1fc8e3314ae343575c1ee7c95b7df6a9
taktiks2 <deathproof.lee@gmail.com>
2023-11-21 23:11:14 +0900

    abc063 a AC

--- abc063/src/bin/a.rs
+++ abc063/src/bin/a.rs
@@ -1,6 +1,14 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        a: i32,
+        b: i32,
     };
+
+    if a + b >= 10 {
+        println!("error");
+    } else {
+        println!("{}", a + b);
+    }
 }
