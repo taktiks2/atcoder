ffb7c7ed92711a8ba625951d4aba08a19730d50c
taktiks2 <deathproof.lee@gmail.com>
2023-11-23 14:20:59 +0900

    typical90 002 AC

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,6 +1,30 @@
-use proconio::{ input };
+use proconio::input;
+
+// bit全探索
 
 fn main() {
     input! {
+        n: usize,
     };
+    if n % 2 == 0 {
+        // 左ビットシフトさせることで2のn乗を計算
+        for i in 0..2 << (n - 1) {
+            let bit = format!("{:0width$b}", i, width = n);
+            let mut ttl = 0;
+            for c in bit.chars() {
+                if c == '0' {
+                    ttl += 1;
+                } else {
+                    ttl -= 1;
+                }
+
+                if ttl < 0 {
+                    break;
+                }
+            }
+            if ttl == 0 {
+                println!("{}", bit.replace('0', "(").replace('1', ")"));
+            }
+        }
+    }
 }
