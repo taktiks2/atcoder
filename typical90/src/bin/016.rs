e21c8fb8e70aeb3742b0c215d57bc49a92fb8a79
taktiks2 <deathproof.lee@gmail.com>
2023-11-26 17:19:33 +0900

    typical90 016 AC

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -1,6 +1,27 @@
-use proconio::{ input };
+use proconio::input;
+
+// 全探索
 
 fn main() {
     input! {
+        n: i64,
+        a: i64,
+        b: i64,
+        c: i64,
     };
+
+    let mut ans: i64 = 10000;
+
+    let l = 9999;
+
+    for i in 0..=l {
+        for j in 0..=l - i {
+            let remain = n - (a * i + b * j);
+            if remain >= 0 && remain % c == 0 {
+                ans = ans.min(i + j + (remain / c));
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
