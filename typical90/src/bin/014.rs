d6f2c258bfc622c24c5b7d6e0b9f46f5b12e46be
taktiks2 <deathproof.lee@gmail.com>
2023-11-26 17:19:19 +0900

    typical 014 AC

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -1,6 +1,22 @@
-use proconio::{ input };
+use proconio::input;
+
+// 貪欲法;
 
 fn main() {
     input! {
+        n: usize,
+        mut a: [i64; n],
+        mut b: [i64; n],
     };
+
+    a.sort();
+    b.sort();
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        ans += (a[i] - b[i]).abs();
+    }
+
+    println!("{}", ans);
 }
