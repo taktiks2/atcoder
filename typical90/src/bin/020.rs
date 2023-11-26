7b4915104f66b2370e20c56117205444441dadb4
taktiks2 <deathproof.lee@gmail.com>
2023-11-26 21:15:09 +0900

    typical90 020 AC

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -1,6 +1,20 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        a: u64,
+        b: u32,
+        c: u64,
     };
+
+    let mut result: u64 = 1;
+    for _ in 1..=b {
+        result *= c;
+    }
+
+    if a < result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
