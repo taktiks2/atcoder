daa8d5537698653481054e345d7fbab9be2e5f6f
taktiks2 <deathproof.lee@gmail.com>
2023-11-27 08:23:06 +0900

    abc330 a AC

--- abc330/src/bin/a.rs
+++ abc330/src/bin/a.rs
@@ -1,6 +1,19 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        l: i32,
+        a: [i32; n],
     };
+
+    let mut cnt = 0;
+
+    for ai in a {
+        if ai >= l {
+            cnt += 1;
+        }
+    }
+
+    println!("{}", cnt)
 }
