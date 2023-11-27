a77e66a091b46ffcc1f3d23f738a3954a2af6164
taktiks2 <deathproof.lee@gmail.com>
2023-11-28 08:15:36 +0900

    abc318 a AC

--- abc318/src/bin/a.rs
+++ abc318/src/bin/a.rs
@@ -1,6 +1,18 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: i32,
+        m: i32,
+        p: i32,
     };
+
+    let remain = n - m;
+
+    if remain < 0 {
+        println!("0");
+        return;
+    }
+
+    println!("{}", (n - m) / p + 1);
 }
