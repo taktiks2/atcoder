06d6ca6361813e328596f8fb7f3415b8084a8104
taktiks2 <deathproof.lee@gmail.com>
2023-11-29 07:45:05 +0900

    abc324 b WA

--- abc324/src/bin/b.rs
+++ abc324/src/bin/b.rs
@@ -1,6 +1,26 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: u64,
     };
+
+    let two = (n as f64).sqrt().ceil() as u32;
+    let three = (n as f64).cbrt().ceil() as u32;
+
+    for i in 0..two {
+        let pow2 = 2u64.pow(i);
+        for j in 0..three {
+            let pow3 = 3u64.pow(j);
+            if pow2 > n / pow3 {
+                break;
+            }
+            if pow2 * pow3 == n {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }
