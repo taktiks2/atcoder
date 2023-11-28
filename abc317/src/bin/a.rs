4351a63b8a885ef3b09a7c71c2718f6b06f5a5c1
taktiks2 <deathproof.lee@gmail.com>
2023-11-29 07:57:32 +0900

    abc317 a AC

--- abc317/src/bin/a.rs
+++ abc317/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        h: i32,
+        x: i32,
+        p: [i32; n],
     };
+
+    for (i, pe) in p.iter().enumerate() {
+        if h + pe >= x {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
