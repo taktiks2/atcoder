202986f48b9d74b599a68fbaf3d72fe64c256722
taktiks2 <deathproof.lee@gmail.com>
2023-12-01 00:02:41 +0900

    abc315 a AC

--- abc315/src/bin/a.rs
+++ abc315/src/bin/a.rs
@@ -1,6 +1,13 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        s: String,
     };
+
+    let chars = ['a', 'i', 'u', 'e', 'o'];
+
+    let ans: String = s.chars().filter(|&c| !chars.contains(&c)).collect();
+
+    println!("{}", ans);
 }
