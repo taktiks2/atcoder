b52d2bcdaa7889f6a3c70b7d460d3815fa499aaa
taktiks2 <deathproof.lee@gmail.com>
2023-12-02 23:25:23 +0900

    abc312 a AC

--- abc312/src/bin/a.rs
+++ abc312/src/bin/a.rs
@@ -1,6 +1,15 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        s: String,
     };
+
+    let list = ["ACE", "BDF", "CEG", "DFA", "EGB", "FAC", "GBD"];
+
+    if list.iter().any(|&item| item == s) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
