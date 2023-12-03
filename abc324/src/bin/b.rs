0640e16f21c6ac9bd3fa62a19722082078d87aaa
taktiks2 <deathproof.lee@gmail.com>
2023-12-03 17:42:25 +0900

    abc324 b AC

--- abc324/src/bin/b.rs
+++ abc324/src/bin/b.rs
@@ -2,25 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: u64,
+        mut n: u64,
     };
 
-    let two = (n as f64).sqrt().ceil() as u32;
-    let three = (n as f64).cbrt().ceil() as u32;
-
-    for i in 0..two {
-        let pow2 = 2u64.pow(i);
-        for j in 0..three {
-            let pow3 = 3u64.pow(j);
-            if pow2 > n / pow3 {
-                break;
-            }
-            if pow2 * pow3 == n {
-                println!("Yes");
-                return;
-            }
+    loop {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            break;
         }
     }
 
-    println!("No");
+    loop {
+        if n % 3 == 0 {
+            n /= 3;
+        } else {
+            break;
+        }
+    }
+
+    if n == 1 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
