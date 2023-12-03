ba7b44cb00baa7d9911f45c6075c77ed18544a33
taktiks2 <deathproof.lee@gmail.com>
2023-12-04 00:37:52 +0900

    abc311 a AC

--- abc311/src/bin/a.rs
+++ abc311/src/bin/a.rs
@@ -1,6 +1,24 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        _: usize,
+        s: String,
     };
+
+    let mut a_flag = false;
+    let mut b_flag = false;
+    let mut c_flag = false;
+    for (i, c) in s.chars().enumerate() {
+        match c {
+            'A' => a_flag = true,
+            'B' => b_flag = true,
+            'C' => c_flag = true,
+            _ => (),
+        }
+        if a_flag && b_flag && c_flag {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
