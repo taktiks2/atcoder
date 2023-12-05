f58d4a97925e6f89b53c0dfd29ba47886f921fcf
taktiks2 <deathproof.lee@gmail.com>
2023-12-06 00:08:05 +0900

    abc307 a AC

--- abc307/src/bin/a.rs
+++ abc307/src/bin/a.rs
@@ -1,6 +1,20 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [i32; n * 7],
     };
+
+    for i in 0..n {
+        let mut total = 0;
+        for j in 0..7 {
+            total += a[i * 7 + j];
+        }
+        if i == n - 1 {
+            println!("{}", total);
+        } else {
+            print!("{} ", total)
+        }
+    }
 }
