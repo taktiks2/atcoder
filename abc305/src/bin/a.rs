3fee42bb85c2e389f8b0f7afaaae39d955a8745b
taktiks2 <deathproof.lee@gmail.com>
2023-12-07 00:28:44 +0900

    abc305 a AC

--- abc305/src/bin/a.rs
+++ abc305/src/bin/a.rs
@@ -1,6 +1,15 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: i32,
     };
+
+    let numbers: Vec<i32> = (0..=100).step_by(5).collect();
+    let ans = numbers
+        .iter()
+        .min_by_key(|&&x| (x - n).abs())
+        .copied()
+        .unwrap();
+    println!("{}", ans)
 }
