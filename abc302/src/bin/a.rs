abe84659dda8df0ddc04eb3453788dbb528d164a
taktiks2 <deathproof.lee@gmail.com>
2023-12-12 23:06:20 +0900

    abc302 a AC

--- abc302/src/bin/a.rs
+++ abc302/src/bin/a.rs
@@ -1,6 +1,15 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        a: usize,
+        b: usize,
     };
+
+    let mut ans = a / b;
+    if a % b != 0 {
+        ans += 1;
+    }
+
+    println!("{}", ans)
 }
