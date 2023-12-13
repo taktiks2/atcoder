5dc4676005837d3bd0001c15bec1e7eb63507bef
taktiks2 <deathproof.lee@gmail.com>
2023-12-13 20:58:45 +0900

    abc331 b AC

--- abc331/src/bin/b.rs
+++ abc331/src/bin/b.rs
@@ -1,6 +1,23 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        s: usize,
+        m: usize,
+        l: usize,
     };
+
+    let mut ans = 1_000_000_000;
+    for i in 0..30 {
+        for j in 0..30 {
+            for k in 0..30 {
+                if i * 6 + j * 8 + k * 12 >= n {
+                    ans = ans.min(i * s + j * m + k * l);
+                }
+            }
+        }
+    }
+
+    println!("{}", ans)
 }
