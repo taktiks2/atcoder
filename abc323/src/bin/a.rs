57a2a2e72d5501f269c99c4d7d0b416c72e5b0de
taktiks2 <deathproof.lee@gmail.com>
2023-12-14 22:52:04 +0900

    abc323 a AC

--- abc323/src/bin/a.rs
+++ abc323/src/bin/a.rs
@@ -1,6 +1,16 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        s: String,
     };
+
+    let mut ans = "Yes";
+    for (i, c) in s.chars().enumerate() {
+        if i % 2 == 1 && c != '0' {
+            ans = "No";
+        }
+    }
+
+    println!("{}", ans);
 }
