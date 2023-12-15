2c012ef9db3473785b20e738f7b5660e8d731aa6
taktiks2 <deathproof.lee@gmail.com>
2023-12-15 21:11:32 +0900

    abc313 a AC

--- abc313/src/bin/a.rs
+++ abc313/src/bin/a.rs
@@ -1,6 +1,22 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        p: [usize; n],
     };
+
+    let mut subs = vec![];
+    for i in 1..n {
+        if p[0] <= p[i] {
+            subs.push(p[i] - p[0])
+        }
+    }
+
+    let ans = match subs.len() {
+        0 => 0,
+        _ => *subs.iter().max().unwrap() + 1,
+    };
+
+    println!("{}", ans)
 }
