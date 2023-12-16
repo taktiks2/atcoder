24ed07fbb39274da61fd78a5122df9030fabb1e8
taktiks2 <deathproof.lee@gmail.com>
2023-12-16 09:43:03 +0900

    abc310 a AC

--- abc310/src/bin/a.rs
+++ abc310/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        p: usize,
+        q: usize,
+        d: [usize; n],
     };
+
+    let min_d = d.iter().min().unwrap();
+    let mut ans = p;
+    if min_d + q <= p {
+        ans = min_d + q;
+    }
+    println!("{}", ans);
 }
