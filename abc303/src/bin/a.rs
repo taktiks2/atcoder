67dd2f44f559f232bcd9fa0a013dd998982555b1
taktiks2 <deathproof.lee@gmail.com>
2023-12-18 22:07:05 +0900

    abc303 a AC

--- abc303/src/bin/a.rs
+++ abc303/src/bin/a.rs
@@ -1,6 +1,25 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        s: String,
+        t: String,
     };
+
+    let vec_s = s.chars().collect::<Vec<char>>();
+    let vec_t = t.chars().collect::<Vec<char>>();
+    let mut ans = "Yes";
+    for i in 0..n {
+        if (vec_s[i] == 'o' || vec_s[i] == '0') && (vec_t[i] == 'o' || vec_t[i] == '0')
+            || (vec_s[i] == 'l' || vec_s[i] == '1') && (vec_t[i] == 'l' || vec_t[i] == '1')
+            || vec_s[i] == vec_t[i]
+        {
+            continue;
+        } else {
+            ans = "No";
+            break;
+        }
+    }
+    println!("{}", ans)
 }
