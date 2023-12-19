fe82dc7ae4a1aa94ffbf6ccb4383db737aaa10d8
taktiks2 <deathproof.lee@gmail.com>
2023-12-19 22:39:00 +0900

    abc306 a AC

--- abc306/src/bin/a.rs
+++ abc306/src/bin/a.rs
@@ -1,6 +1,12 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        _: usize,
+        s: String,
     };
+
+    for c in s.chars() {
+        print!("{}{}", c, c)
+    }
 }
