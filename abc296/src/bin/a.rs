2933751bb77f72d920708723d73e4967e06e0210
taktiks2 <deathproof.lee@gmail.com>
2023-12-20 22:31:24 +0900

    abc296 a AC

--- abc296/src/bin/a.rs
+++ abc296/src/bin/a.rs
@@ -1,6 +1,17 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        _: usize,
+        s: String,
     };
+
+    let mut ans = "Yes";
+    for c2 in s.chars().collect::<Vec<char>>().windows(2) {
+        if c2[0] == c2[1] {
+            ans = "No";
+        }
+    }
+
+    println!("{}", ans)
 }
