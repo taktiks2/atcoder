80e779a08332bd13d42ee3a5a13aa463ae720bd4
taktiks2 <deathproof.lee@gmail.com>
2023-12-22 22:10:09 +0900

    abc294 a AC

--- abc294/src/bin/a.rs
+++ abc294/src/bin/a.rs
@@ -1,6 +1,14 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n],
     };
+
+    let evens: Vec<usize> = a.into_iter().filter(|ai| ai % 2 == 0).collect();
+
+    let evens_str: Vec<String> = evens.iter().map(|ai| ai.to_string()).collect();
+
+    println!("{}", evens_str.join(" "))
 }
