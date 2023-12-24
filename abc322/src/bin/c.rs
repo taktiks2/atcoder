2cc61e2b34a835e42cf5e5301cedb912fb8d35af
taktiks2 <deathproof.lee@gmail.com>
2023-12-24 18:50:30 +0900

    abc322 c TLE

--- abc322/src/bin/c.rs
+++ abc322/src/bin/c.rs
@@ -1,6 +1,25 @@
-use proconio::{ input };
+use proconio::input;
+use std::cmp::Ordering;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; m],
     };
+
+    for i in 1..=n {
+        match i.cmp(&a[0]) {
+            Ordering::Less => {
+                println!("{}", a[0] - i);
+            }
+            Ordering::Equal => {
+                println!("0");
+            }
+            Ordering::Greater => {
+                a.remove(0);
+                println!("{}", a[0] - i);
+            }
+        }
+    }
 }
