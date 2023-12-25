5960b207a6d7c7d53b1e4d4b2d1be99d80396313
taktiks2 <deathproof.lee@gmail.com>
2023-12-25 22:50:25 +0900

    abc295 a AC

--- abc295/src/bin/a.rs
+++ abc295/src/bin/a.rs
@@ -1,6 +1,18 @@
-use proconio::{ input };
+use proconio::input;
+
+const WORDS: [&str; 5] = ["and", "not", "that", "the", "you"];
 
 fn main() {
     input! {
+        n: usize,
+        w: [String; n],
     };
+
+    for word in w {
+        if WORDS.iter().any(|&item| item == word) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
