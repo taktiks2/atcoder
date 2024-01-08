f1b7063b6cb17280dfc3878f2b74f08af51baa54
taktiks2 <deathproof.lee@gmail.com>
2024-01-08 16:04:48 +0900

    typicalk90 038 AC

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -1,6 +1,28 @@
-use proconio::{ input };
+use proconio::input;
+
+fn gcd(a: u128, b: u128) -> u128 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u128, b: u128) -> u128 {
+    a / gcd(a, b) * b
+}
 
 fn main() {
     input! {
+        a: u128,
+        b: u128,
     };
+
+    let ans = lcm(a, b);
+
+    if ans > 1_000_000_000_000_000_000 {
+        println!("Large")
+    } else {
+        println!("{}", ans)
+    }
 }
