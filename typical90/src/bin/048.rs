008fd0e75bbbe60e6f7bd3eae1b0428ea97c133f
taktiks2 <deathproof.lee@gmail.com>
2024-01-18 23:44:15 +0900

    typical90 048 AC

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -1,6 +1,22 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
     };
+
+    let mut nums: Vec<usize> = vec![];
+    for _ in 0..n {
+        input! {
+            a: usize,
+            b: usize,
+        };
+
+        nums.push(b);
+        nums.push(a - b);
+    }
+
+    nums.sort_by(|a, b| b.cmp(a));
+    println!("{:?}", nums.iter().take(k).sum::<usize>())
 }
