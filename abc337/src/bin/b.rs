db2270feaa423ffcdf8b67c7660ce3fe53fb4da5
taktiks2 <deathproof.lee@gmail.com>
2024-01-25 23:05:37 +0900

    abc337 b AC

--- abc337/src/bin/b.rs
+++ abc337/src/bin/b.rs
@@ -1,6 +1,26 @@
-use proconio::{ input };
+use proconio::input;
+
+fn create_str(c: char, n: usize) -> String {
+    std::iter::repeat(c).take(n).collect()
+}
 
 fn main() {
     input! {
+        s: String,
     };
+
+    let len = s.len();
+
+    for a in 0..=len {
+        for b in 0..=len {
+            for c in 0..=len {
+                let str = create_str('A', a) + &create_str('B', b) + &create_str('C', c);
+                if str == s {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+    println!("No");
 }
