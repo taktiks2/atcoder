a806ac0078d4be082de17a34da4118297968dc1b
taktiks2 <deathproof.lee@gmail.com>
2024-06-02 19:45:34 +0900

    tessoku-book b04 ~ a05 AC

--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,6 +1,16 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: String
     };
+
+    let mut ans = 0;
+    for (i, c) in n.chars().enumerate() {
+        if c == '1' {
+            ans += 1 << (n.len() - i - 1);
+        }
+    }
+
+    println!("{}", ans);
 }
