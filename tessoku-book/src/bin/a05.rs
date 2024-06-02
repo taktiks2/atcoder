657b5912a029e2b8bca0737d7053bd5cb62b28fa
taktiks2 <deathproof.lee@gmail.com>
2024-06-02 19:46:36 +0900

    tessoku-book a05 ~ a06 AC

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,6 +1,21 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: i32,
+        k: i32
     };
+
+    let mut ans = 0;
+
+    for i in 1..=n {
+        for j in 1..=n {
+            let tmp = k - i - j;
+            if tmp >= 1 && tmp <= n {
+                ans += 1
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,6 +1,29 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        q: usize,
+        a: [usize; n],
     };
+
+    let mut acc_list = vec![];
+    let mut acc = 0;
+    for ai in a {
+        acc += ai;
+        acc_list.push(acc);
+    }
+
+    for _ in 0..q {
+        input! {
+            l: usize,
+            r: usize
+        };
+
+        if l == 1 {
+            println!("{}", acc_list[r - 1]);
+            continue;
+        }
+        println!("{}", acc_list[r - 1] - acc_list[l - 2])
+    }
 }
