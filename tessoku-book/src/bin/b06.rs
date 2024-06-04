48a7a1c304973fdd5e8b91fcbb451a3f91c4dcb6
taktiks2 <deathproof.lee@gmail.com>
2024-06-04 23:55:44 +0900

    tessoku-book b06 AC

--- tessoku-book/src/bin/b06.rs
+++ tessoku-book/src/bin/b06.rs
@@ -1,6 +1,40 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n],
+        q: usize,
+        lr: [(usize, usize); q]
     };
+
+    let acc_list = a
+        .iter()
+        .scan(0, |acc, &x| {
+            *acc += x;
+            Some(*acc)
+        })
+        .collect::<Vec<usize>>();
+
+    for (l, r) in lr {
+        let acc_part = acc_list[r - 1] - if l == 1 { 0 } else { acc_list[l - 2] };
+        let all = r - l + 1;
+        let half = all / 2;
+
+        if all % 2 == 0 {
+            if acc_part > half {
+                println!("win");
+            } else if acc_part == half {
+                println!("draw");
+            } else {
+                println!("lose");
+            }
+        } else {
+            if acc_part >= half + 1 {
+                println!("win");
+            } else {
+                println!("lose");
+            }
+        }
+    }
 }
