5c30b092720efb0506e314a44a27501b9e0806d5
taktiks2 <deathproof.lee@gmail.com>
2024-06-19 22:21:06 +0900

    tessoku-book b07 AC

--- tessoku-book/src/bin/b07.rs
+++ tessoku-book/src/bin/b07.rs
@@ -1,6 +1,22 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        t: usize,
+        n: usize,
+        lr: [(usize, usize); n],
     };
+
+    let mut arr: Vec<i32> = vec![0; t + 2];
+
+    for (l, r) in lr {
+        arr[l + 1] += 1;
+        arr[r + 1] -= 1
+    }
+
+    let mut acc = vec![0; t + 1];
+    for i in 1..=t {
+        acc[i] = acc[i - 1] + arr[i];
+        println!("{}", acc[i])
+    }
 }
