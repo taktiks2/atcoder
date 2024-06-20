f935dc8d6323ae4136d6ca8a419c645092c58cf4
taktiks2 <deathproof.lee@gmail.com>
2024-06-20 23:23:16 +0900

    tessoku-book_a08_WIP

--- tessoku-book/src/bin/a08.rs
+++ tessoku-book/src/bin/a08.rs
@@ -1,6 +1,19 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        x: [[usize; w]; h],
+        q: usize,
+        abcd: [[usize; 4]; q]
     };
+
+    let mut acc = vec![vec![0; w]; h];
+
+    // 横方向のacc
+
+    // 縦方向のacc
+
+    // 総和を計算
 }
