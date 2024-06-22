970957a6e97b8655800df9e8c075184a2afc094c
taktiks2 <deathproof.lee@gmail.com>
2024-06-22 23:26:33 +0900

    tessoku-book_a08_AC

--- tessoku-book/src/bin/a08.rs
+++ tessoku-book/src/bin/a08.rs
@@ -9,11 +9,31 @@ fn main() {
         abcd: [[usize; 4]; q]
     };
 
-    let mut acc = vec![vec![0; w]; h];
+    let mut acc = vec![vec![0; w + 2]; h + 2];
 
     // 横方向のacc
+    for hi in 1..=h {
+        for wj in 1..=w {
+            acc[hi][wj] = acc[hi][wj - 1] + x[hi - 1][wj - 1]
+        }
+    }
 
     // 縦方向のacc
+    for wi in 1..=w {
+        for hj in 1..=h {
+            acc[hj][wi] = acc[hj - 1][wi] + acc[hj][wi]
+        }
+    }
 
     // 総和を計算
+    for l in abcd {
+        let a = l[0];
+        let b = l[1];
+        let c = l[2];
+        let d = l[3];
+        println!(
+            "{}",
+            acc[c][d] + acc[a - 1][b - 1] - acc[c][b - 1] - acc[a - 1][d]
+        )
+    }
 }
