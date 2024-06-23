b86d5ec8ba012ec1f1818d44fe2c432ed084d27f
taktiks2 <deathproof.lee@gmail.com>
2024-06-23 15:48:25 +0900

    tessoku-book_b09_AC

--- tessoku-book/src/bin/b09.rs
+++ tessoku-book/src/bin/b09.rs
@@ -1,6 +1,41 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        abcd: [(usize, usize, usize, usize); n]
     };
+
+    const SIZE: usize = 1501;
+
+    let mut acc = vec![vec![0; SIZE]; SIZE];
+    for (a, b, c, d) in abcd {
+        acc[a][b] += 1;
+        acc[c][d] += 1;
+        acc[a][d] -= 1;
+        acc[c][b] -= 1;
+    }
+
+    for i in 0..SIZE {
+        for j in 1..SIZE {
+            acc[i][j] += acc[i][j - 1]
+        }
+    }
+
+    for j in 0..SIZE {
+        for i in 1..SIZE {
+            acc[i][j] += acc[i - 1][j]
+        }
+    }
+
+    let mut cnt = 0;
+    for i in 0..SIZE - 1 {
+        for j in 0..SIZE - 1 {
+            if acc[i][j] > 0 {
+                cnt += 1
+            }
+        }
+    }
+
+    println!("{}", cnt)
 }
