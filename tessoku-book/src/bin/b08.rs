5dfa863a2b1d2a061d5292593838b2a17df27d9b
taktiks2 <deathproof.lee@gmail.com>
2024-06-23 11:54:36 +0900

    tessoku-book_b08_AC

--- tessoku-book/src/bin/b08.rs
+++ tessoku-book/src/bin/b08.rs
@@ -1,6 +1,40 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        z: [[usize; 2]; n],
+        q: usize,
+        e: [[usize; 4]; q]
     };
+
+    const SIZE: usize = 1509;
+
+    let mut plane = vec![vec![0; SIZE]; SIZE];
+
+    for l in z {
+        plane[l[1]][l[0]] += 1
+    }
+
+    let mut acc = vec![vec![0; SIZE]; SIZE];
+    for i in 1..SIZE {
+        for j in 1..SIZE {
+            acc[i][j] = acc[i][j - 1] + plane[i][j]
+        }
+    }
+
+    for i in 1..SIZE {
+        for j in 1..SIZE {
+            acc[j][i] = acc[j - 1][i] + acc[j][i]
+        }
+    }
+
+    for l in e {
+        let (a, b, c, d) = (l[0], l[1], l[2], l[3]);
+
+        println!(
+            "{}",
+            acc[d][c] + acc[b - 1][a - 1] - acc[b - 1][c] - acc[d][a - 1]
+        )
+    }
 }
