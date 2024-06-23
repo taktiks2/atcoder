90aa0700a1cce9e56ac7a7d46186423fb8964ba3
taktiks2 <deathproof.lee@gmail.com>
2024-06-23 20:07:12 +0900

    tessoku-book_a11_AC

--- tessoku-book/src/bin/a11.rs
+++ tessoku-book/src/bin/a11.rs
@@ -1,6 +1,30 @@
-use proconio::{ input };
+use proconio::input;
+use std::cmp::Ordering;
+
+fn search(arr: &[i32], target: i32) -> Option<usize> {
+    let mut l = 0;
+    let mut r = arr.len();
+
+    while l <= r {
+        let m = (l + r) / 2;
+        match arr[m].cmp(&target) {
+            Ordering::Less => l = m + 1,
+            Ordering::Equal => return Some(m),
+            Ordering::Greater => r = m - 1,
+        }
+    }
+    None
+}
 
 fn main() {
     input! {
+        n: usize,
+        x: i32,
+        a: [i32; n]
     };
+
+    match search(&a, x) {
+        Some(i) => println!("{}", i + 1),
+        None => println!("There is no target"),
+    }
 }
