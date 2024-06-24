196b8326afc4ef389972f24b7c5b2e57cd31898e
taktiks2 <deathproof.lee@gmail.com>
2024-06-24 22:29:38 +0900

    tessoku-book_b11_AC

--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -1,6 +1,17 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        mut a: [usize; n],
+        q: usize,
+        x: [usize; q]
     };
+
+    a.sort();
+
+    x.iter().for_each(|x| match a.binary_search(x) {
+        Ok(index) => println!("{}", index),
+        Err(index) => println!("{}", index),
+    })
 }
