d5128925cee460cdc43e7daa1765859788c2efbd
taktiks2 <deathproof.lee@gmail.com>
2024-06-25 23:00:44 +0900

    tessoku-book_a12_AC

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,6 +1,27 @@
-use proconio::{ input };
+use proconio::input;
+
+fn check(arr: &[usize], mid: usize, target: usize) -> bool {
+    let mut sum = 0;
+    arr.iter().for_each(|&x| sum += mid / x);
+    sum >= target
+}
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
     };
+
+    let mut left = 0;
+    let mut right = 1_000_000_000;
+
+    while left < right {
+        let mid = (left + right) / 2;
+        match check(&a, mid, k) {
+            true => right = mid,
+            false => left = mid + 1,
+        }
+    }
+    println!("{}", left)
 }
