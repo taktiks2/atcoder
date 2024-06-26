4f5b3120554f9a0bf6f5b63ec2ff0ce896286460
taktiks2 <deathproof.lee@gmail.com>
2024-06-26 22:35:15 +0900

    tessoku-book_b12_AC

--- tessoku-book/src/bin/b12.rs
+++ tessoku-book/src/bin/b12.rs
@@ -1,6 +1,28 @@
-use proconio::{ input };
+use proconio::input;
+
+fn check(mid: f64, target: f64) -> bool {
+    let x = mid.powi(3) + mid;
+    if x <= target {
+        return true;
+    }
+    false
+}
 
 fn main() {
     input! {
+        n: f64,
     };
+
+    let mut left: f64 = 0.0;
+    let mut right: f64 = 100.0;
+
+    while right - left > 0.001 {
+        let mid: f64 = (left + right) / 2.0;
+        match check(mid, n) {
+            true => left = mid,
+            false => right = mid,
+        }
+    }
+
+    println!("{}", left)
 }
