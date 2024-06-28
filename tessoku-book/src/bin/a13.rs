2d43794014ce53548477c8a0bcf89fcec6281e1e
taktiks2 <deathproof.lee@gmail.com>
2024-06-28 23:16:57 +0900

    tessoku-book_a13_AC

--- tessoku-book/src/bin/a13.rs
+++ tessoku-book/src/bin/a13.rs
@@ -4,34 +4,25 @@ fn main() {
     input! {
         n: usize,
         k: i64,
+        a: [i64; n]
     };
 
-    let mut a: Vec<i64> = vec![0; n + 9];
-    for i in 1..n {
-        input! {
-            x: i64,
-        };
-        a[i] = x
-    }
-
-    let mut r = vec![0; n + 9];
+    let mut r = vec![0; n];
     // しゃくとり法
-    for i in 1..n {
+    for i in 0..n {
         // スタート地点決め
-        if i == 1 {
-            r[i] = 1
-        } else {
+        if i > 0 {
             r[i] = r[i - 1]
         }
 
-        while r[i] < n && a[r[i] + 1] - a[i] <= k {
+        while r[i] < n - 1 && a[r[i] + 1] - a[i] <= k {
             r[i] += 1
         }
     }
 
     let mut ans = 0;
-    for i in 1..n {
-        ans += r[i] - i
+    for (i, q) in r.iter().enumerate() {
+        ans += q - i
     }
 
     println!("{}", ans)
