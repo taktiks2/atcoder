2aff959f605237340bacfa0ebaa2e0d1bdb5839f
taktiks2 <deathproof.lee@gmail.com>
2024-06-29 14:02:30 +0900

    tessoku-book_b13_AC

--- tessoku-book/src/bin/b13.rs
+++ tessoku-book/src/bin/b13.rs
@@ -1,6 +1,26 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
     };
+
+    let mut r = vec![0; n];
+
+    for i in 0..n {
+        let mut acc = 0;
+        r[i] = i;
+        while r[i] < n && acc + a[r[i]] <= k {
+            acc += a[r[i]];
+            r[i] += 1
+        }
+    }
+
+    let mut ans = 0;
+    for (i, q) in r.iter().enumerate() {
+        ans += q - i
+    }
+    println!("{}", ans)
 }
