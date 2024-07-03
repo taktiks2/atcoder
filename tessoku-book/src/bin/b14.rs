dd65def596fd54cee8bf041e3d21554cf559a7bc
taktiks2 <deathproof.lee@gmail.com>
2024-07-03 23:30:49 +0900

    tessoku-book_b14_AC

--- tessoku-book/src/bin/b14.rs
+++ tessoku-book/src/bin/b14.rs
@@ -1,6 +1,52 @@
-use proconio::{ input };
+use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
     };
+
+    let (a1, a2) = a.split_at(n / 2);
+
+    let mut p: Vec<usize> = vec![];
+    for i in 0..(1 << a1.len()) {
+        let mut sum = 0;
+        for j in 0..a1.len() {
+            let divider = 1 << j;
+            if (i / divider) % 2 == 1 {
+                sum += a1[j]
+            }
+        }
+        p.push(sum)
+    }
+
+    let mut q: Vec<usize> = vec![];
+    for i in 0..(1 << a2.len()) {
+        let mut sum = 0;
+        for j in 0..a2.len() {
+            let divider = 1 << j;
+            if (i / divider) % 2 == 1 {
+                sum += a2[j]
+            }
+        }
+        q.push(sum)
+    }
+    q.sort();
+
+    for pe in p {
+        if k < pe {
+            continue;
+        }
+        let target = k - pe;
+        let index = q.binary_search(&target);
+        match index {
+            Ok(_) => {
+                println!("Yes");
+                return;
+            }
+            Err(_) => continue,
+        }
+    }
+    println!("No")
 }
